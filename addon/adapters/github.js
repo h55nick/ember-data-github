93b5369de0a739b8dd5539bc4a09427097094896
Nick Blanchet <h55nick@gmail.com>
2015-11-17 07:32:15 -0500

    allow adapater to pull from torii-github-oauth2 directly

--- addon/adapters/github.js
+++ addon/adapters/github.js
@@ -1,15 +1,29 @@
 import DS from 'ember-data';
 import Ember from 'ember';
 
+const alias = Ember.computed.alias;
+const service = Ember.inject.service;
+const githubUrl = 'https://api.github.com';
+
 export default DS.RESTAdapter.extend({
-  session: Ember.inject.service(),
-  host: 'https://api.github.com',
-  headers: Ember.computed('session.githubAccessToken', function() {
-    var headers = {};
-    if(this.get('session.githubAccessToken')) {
-      headers.Authorization = `token ${this.get('session.githubAccessToken')}`;
+  session: service(),
+  host: githubUrl,
+  authorizationCode: alias('session.data.authenticated.authorizationCode'),
+  githubAccessToken: alias('session.githubAccessToken'), // backwards compatibility
+  token: Ember.computed('githubAccessToken', 'authorizationCode', function(){
+    // backwards compatibility
+    if(this.get('githubAccessToken')){
+     return this.get('githubAccessToken');
     }
-    return headers;
+
+    if(this.get('authorizationCode')){
+      return this.get('authorizationCode');
+    }
+    return true;
+  }),
+
+  headers: Ember.computed('token', function() {
+    return { Authorization: `token ${this.get('token')}` };
   }),
   pathForType: function(type) {
     return Ember.String.camelize(Ember.String.pluralize(type.replace('github','')));
